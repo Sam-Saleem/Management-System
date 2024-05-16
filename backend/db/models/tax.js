f8339e3d6ed00534738c5467fca0921077ed71b6
Sehar <seharsaleem08@gmail.com>
2024-05-16 23:27:15 +0500

    Added the tax table.

---
+++ backend/db/models/tax.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Tax extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tax.init(
+    {
+      minIncome: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      maxIncome: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      taxRate: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Tax",
+
+      // Define a unique constraint for the combination of minIncome and maxIncome
+      indexes: [
+        {
+          unique: true,
+          fields: ["minIncome", "maxIncome"],
+        },
+      ],
+    }
+  );
+  return Tax;
+};
